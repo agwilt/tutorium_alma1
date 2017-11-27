8755ff47580443466b9a6cf45d9daa93165f0855
Andreas Gwilt <andreas@gwilt.net>
2017-11-27 16:54:12 +0100

    Add array-fibonacci-functions

--- Blatt7Aufg1.c
+++ Blatt7Aufg1.c
@@ -13,6 +13,27 @@ int rec_fib(int n)
 	}
 }
 
+void slow_array(int n, int *zahlen)
+{
+	for (int i=1; i<=n; ++i) {
+		zahlen[i-1] = rec_fib(i);
+		printf("fibonacci(%d) = %d\n", i, zahlen[i-1]);
+	}
+}
+
+void fast_array(int n, int *zahlen)
+{
+	/* Wir gehen davon aus, dass n >= 2. */
+	zahlen[0] = zahlen[1] = 1;
+	printf("fibonacci(0) = 1\n");
+	printf("fibonacci(1) = 1\n");
+
+	for (int i=2; i<n; ++i) {
+		zahlen[i] = zahlen[i-2] + zahlen[i-1];
+		printf("fibonacci(%d) = %d\n", i+1, zahlen[i]);
+	}
+}
+
 int main(int argc, char *argv[])
 {
 	if (argc<=1) {
@@ -21,7 +42,13 @@ int main(int argc, char *argv[])
 	}
 
 	int n = atoi(argv[1]);
-	printf("%d\n", rec_fib(n));
+/*	printf("%d\n", rec_fib(n)); */
+	int zahlen[n];
+
+	printf("Langsame Version:\n");
+	slow_array(n, zahlen);
+	printf("\nSchnelle Version:\n");
+	fast_array(n, zahlen);
 
 	return 0;
 }
